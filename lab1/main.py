32b5210b5295a69b5bac2c00558993aa1fa4d142
RobertLMcCrary <rlmccrary1210@gmail.com>
2026-09-03 11:31:45 -0400

    lab1 done

--- lab1/main.py
+++ lab1/main.py
@@ -3,15 +3,20 @@
 # Student Name: Robert McCrary
 # Assignment Number: Lab1
 # Due Date: 08/27/2026
+# Editor: Neovim
+# Env: Nix
 
+# take user input as a float
 width = float(input("Room Width: "))
 length = float(input("Room Length: "))
 cost = float(input("Cost per Sq. Foot: "))
 
+# compute values
 tax = 0.07 * cost
 total_cost_pre_tax = (width * length) * cost
 total_cost = (tax + cost) * (width * length)
 
+# display correct output
 print(f"Square feet: {width * length}")
 print(f"Flooring: {total_cost_pre_tax}")
 print(f"Tax: {tax}")
