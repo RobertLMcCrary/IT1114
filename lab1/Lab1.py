8a8aa5823e053097d8d96d09970dab196bd2adf4
RobertLMcCrary <rlmccrary1210@gmail.com>
2026-09-03 11:45:14 -0400

    fixed error

--- lab1/Lab1.py
+++ lab1/Lab1.py
@@ -2,7 +2,7 @@
 # Course: IT1114 and IT1114L Section 01
 # Student Name: Robert McCrary
 # Assignment Number: Lab1
-# Due Date: 08/27/2026
+# Due Date: 09/5/2026
 # Editor: Neovim
 # Env: Nix
 
@@ -12,12 +12,14 @@ length = float(input("Room Length: "))
 cost = float(input("Cost per Sq. Foot: "))
 
 # compute values
-tax = 0.07 * cost
-total_cost_pre_tax = (width * length) * cost
-total_cost = (tax + cost) * (width * length)
+area = width * length
+total_cost_pre_tax = area * cost
+tax = total_cost_pre_tax * 0.07
+total_cost = (tax + cost) * area
 
 # display correct output
 print(f"Square feet: {width * length}")
 print(f"Flooring: {total_cost_pre_tax}")
 print(f"Tax: {tax}")
 print(f"Total: {total_cost}")
+
