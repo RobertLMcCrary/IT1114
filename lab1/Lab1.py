6653a54510fb59abfcbaaffda7c0a6a28fd8fe19
RobertLMcCrary <rlmccrary1210@gmail.com>
2026-09-03 12:35:42 -0400

    auto grader finally passed

--- lab1/Lab1.py
+++ lab1/Lab1.py
@@ -4,7 +4,8 @@
 # Assignment Number: Lab1
 # Due Date: 09/5/2026
 # Editor: Neovim
-# Env: Nix
+# Environment: Nix
+# GitHub: https://github.com/RobertLMcCrary/IT1114
 
 # take user input as a float
 width = float(input("Room Width: "))
@@ -12,14 +13,13 @@ length = float(input("Room Length: "))
 cost = float(input("Cost per Sq. Foot: "))
 
 # compute values
-area = width * length
-total_cost_pre_tax = area * cost
-tax = total_cost_pre_tax * 0.07
-total_cost = (tax + cost) * area
+sq_ft = length * width
+cost_pre_tax = cost * sq_ft
+tax = cost_pre_tax * 0.07
+cost_post_tax = cost_pre_tax + tax
 
 # display correct output
-print(f"Square feet: {width * length}")
-print(f"Flooring: {total_cost_pre_tax}")
+print(f"Square feet: {sq_ft}")
+print(f"Flooring: {cost_pre_tax}")
 print(f"Tax: {tax}")
-print(f"Total: {total_cost}")
-
+print(f"Total: {cost_post_tax}")
